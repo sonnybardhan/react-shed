33b3ead0a6a0ff80d3d3e1f551a4418836cf2204
abhirup bardhan <bardhan.abhirup@gmail.com>
2022-07-03 17:23:00 +0530

    complete assignment

--- src/TipCalculator.js
+++ src/TipCalculator.js
@@ -5,52 +5,43 @@ const calculatetip = (bill, tipPercent) => {
 };
 
 export default function TipCalculator() {
-  // Write your code here.
   const [bill, setBill] = useState(50);
   const [tipPercent, setTipPercent] = useState(18);
   const [numPeople, setNumPeople] = useState(1);
-  const [totalTip, setTotalTip] = useState('');
-  const [tipPerPerson, setTipPerPerson] = useState('');
 
-  useEffect(() => {
-    if (Number(bill) && Number(tipPercent) && Number(numPeople)) {
-      let tip = (bill * tipPercent * 0.01).toFixed(2);
-      setTotalTip('$' + tip);
-      let perHead = (tip / numPeople).toFixed(2);
-      setTipPerPerson('$' + perHead);
-    } else {
-      setTotalTip('-');
-      setTipPerPerson('-');
-    }
-  }, [bill, tipPercent, numPeople]);
+  let tip = (bill * tipPercent * 0.01).toFixed(2);
+  let perHead = (tip / numPeople).toFixed(2);
 
   return (
     <>
-      <div>
+      <>
         <label htmlFor='bill'>Bill</label>
         <input
           type='number'
           id='bill'
+          min='0'
           value={bill}
-          onChange={(e) => setBill(Number(e.target.value))}
+          onChange={(e) => setBill(parseInt(e.target.value))}
         />
         <label htmlFor='tipPercentage'>Tip Percentage</label>
         <input
           type='number'
           id='tipPercentage'
+          min='0'
           value={tipPercent}
-          onChange={(e) => setTipPercent(Number(e.target.value))}
+          onChange={(e) => setTipPercent(parseInt(e.target.value))}
         />
         <label htmlFor='num-people'>Number of People</label>
         <input
           type='number'
           id='num-people'
+          min='1'
           value={numPeople}
-          onChange={(e) => setNumPeople(Number(e.target.value))}
+          onChange={(e) => setNumPeople(parseInt(e.target.value))}
         />
-      </div>
-      <p>Total Tip: {totalTip}</p>
-      <p>Tip Per Person: {tipPerPerson}</p>
+      </>
+      <p>Total Tip: {isNaN(tip) ? '-' : `$${tip}`} </p>
+      <p>Tip Per Person: {isNaN(perHead) ? '-' : `$${perHead}`}</p>
     </>
   );
 }
